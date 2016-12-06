d11579b75304afc566fe564058d04cf21d2a7a0a
Allen Shaw <allen@JoineryHQ.com>
2016-12-05 19:57:22 -0600

    fix zendesk #10530: Improve yahrzeit search by providing a report.
    Reference: https://pogstone.zendesk.com/agent/tickets/10530

--- CRM/Jmanage/Form/Report/UpcomingYahrzeits.php
+++ CRM/Jmanage/Form/Report/UpcomingYahrzeits.php
@@ -8,7 +8,7 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
 
   protected $_summary = NULL;
 
-  protected $_customGroupExtends = array('Relationship', 'Individual');
+  protected $_customGroupExtends = array('Relationship', 'Individual', 'Contact');
   protected $_customGroupGroupBy = FALSE;
 
   protected $_yahrzeit_table;
@@ -28,7 +28,6 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
 
     $this->_columns = array(
       'civicrm_contact' => array(
-//        'dao' => 'CRM_Contact_DAO_Contact',
         'fields' => array(
           'sort_name' => array(
             'title' => ts('Deceased Name (sortable)'),
@@ -73,10 +72,35 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
             'required' => TRUE,
           ),
         ),
+        'filters' => array(
+          'gender_id' => array(
+            'title' => ts('Mourner gender'),
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Contact_BAO_Contact::buildOptions('gender_id'),
+            'type' => CRM_Utils_Type::T_INT,
+          ),
+          'preferred_communication_method' => array(
+            'title' => ts('Mourner preferred communication method'),
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Contact_BAO_Contact::buildOptions('preferred_communication_method'),
+            'type' => CRM_Utils_Type::T_INT,
+          ),
+          'is_deceased' => array(
+            'title' => ts('Mourner is deceased'),
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Contact_BAO_Contact::buildOptions('is_deceased'),
+            'type' => CRM_Utils_Type::T_INT,
+          ),
+          'has_mourner' => array(
+            'title' => ts('Has Mourner'),
+            'operatorType' => CRM_Report_Form::OP_SELECT,
+            'options' => array(-1 => '(Any)', 1 => 'Yes', 0 => 'No'),
+            'type' => CRM_Utils_Type::T_INT,
+          ),
+        ),
         'grouping' => 'contact-mourner-fields',
       ),
       'civicrm_contact_household' => array(
-//        'dao' => 'CRM_Contact_DAO_Contact',
         'fields' => array(
           'display_name' => array(
             'title' => ts('Mourner Household Name'),
@@ -90,7 +114,6 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
         'grouping' => 'contact-mourner-fields',
       ),
       'civicrm_address' => array(
-//        'dao' => 'CRM_Core_DAO_Address',
         'fields' => array(
           'street_address' => array(
             'title' => ts('Street Address'),
@@ -116,7 +139,6 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
         'grouping' => 'contact-mourner-fields',
       ),
       'civicrm_email' => array(
-//        'dao' => 'CRM_Core_DAO_Email',
         'fields' => array(
           'email' => array(
             'title' => ts('Email'),
@@ -126,7 +148,6 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
         'grouping' => 'contact-mourner-fields',
       ),
       'civicrm_phone' => array(
-//        'dao' => 'CRM_Core_DAO_phone',
         'fields' => array(
           'phone' => array(
             'title' => ts('Phone'),
@@ -194,6 +215,38 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
             'default' => TRUE,
           ),
         ),
+        'filters' => array(
+          'yahrzeit_date' => array(
+            'title' => ts('Yahrzeit Date - Evening'),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+          'yahrzeit_date_morning' => array(
+            'title' => ts('Yahrzeit Date - Morning'),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+          'yahrzeit_erev_shabbat_before' => array(
+            'title' => ts('Friday Night Before Yahrzeit'),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+          'yahrzeit_erev_shabbat_after' => array(
+            'title' => ts('Friday Night After Yahrzeit'),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+          'yahrzeit_shabbat_morning_before' => array(
+            'title' => ts('Saturday Morning Before Yahrzeit '),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+          'yahrzeit_shabbat_morning_after' => array(
+            'title' => ts('Saturday Morning After Yahrzeit'),
+            'operatorType' => CRM_Report_Form::OP_DATE,
+            'type' => CRM_Utils_Type::T_DATE,
+          ),
+        ),
       ),
       'civicrm_relationship' => array(
         'fields' => array(
@@ -234,52 +287,16 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
     );
 
     $this->_groupFilter = TRUE;
-    $this->_tagFilter = TRUE;
     parent::__construct();
 
+    // Change label for 'groups' filter
+    $this->_columns['civicrm_group']['filters']['gid']['title'] = ts('Mourner group(s)');
 
-
-  // FIXME: custom fields to check by 'default':
-  // Deceased Hebrew Name (Religious: "Hebrew Name")
-  // Hebrew Date of Death (Hebrew Calendar Demographics: "Hebrew Date of Death")
-  // Mourner Preference (Yahrzeit Details: "Mourner observes the English date")
-  // Plaque? (Memorial Plaque Info: "Has Plaque")
-  // Plaque Location (Memorial Plaque Info: "Plaque Location")
-
-
-
-    dsm($this->_columns, 'columns');
   }
 
   function preProcess() {
     $this->assign('reportTitle', ts('Membership Detail Report'));
     parent::preProcess();
-  }
-
-  function _fixme_deleteme_select() {
-    $select = $this->_columnHeaders = array();
-
-    foreach ($this->_columns as $tableName => $table) {
-      if (array_key_exists('fields', $table)) {
-        foreach ($table['fields'] as $fieldName => $field) {
-          if (CRM_Utils_Array::value('required', $field) ||
-            CRM_Utils_Array::value($fieldName, $this->_params['fields'])
-          ) {
-            if ($tableName == 'civicrm_address') {
-              $this->_addressField = TRUE;
-            }
-            elseif ($tableName == 'civicrm_email') {
-              $this->_emailField = TRUE;
-            }
-            $select[] = "{$field['dbAlias']} as {$tableName}_{$fieldName}";
-            $this->_columnHeaders["{$tableName}_{$fieldName}"]['title'] = $field['title'];
-            $this->_columnHeaders["{$tableName}_{$fieldName}"]['type'] = CRM_Utils_Array::value('type', $field);
-          }
-        }
-      }
-    }
-
-    $this->_select = "SELECT " . implode(', ', $select) . " ";
   }
 
   function from() {
@@ -298,9 +315,10 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
       || $this->isTableSelected('civicrm_address')
     ) {
       $this->_from .= "
-         INNER JOIN civicrm_contact {$this->_aliases['civicrm_contact_mourner']}
+         LEFT JOIN civicrm_contact {$this->_aliases['civicrm_contact_mourner']}
               ON {$this->_aliases[$this->_yahrzeit_table]}.mourner_contact_id =
                  {$this->_aliases['civicrm_contact_mourner']}.id
+                 AND NOT {$this->_aliases['civicrm_contact_mourner']}.is_deleted
        ";
     }
     if ($this->isTableSelected('civicrm_email')) {
@@ -363,25 +381,30 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
 
   function where() {
     $clauses = array();
+    $params = $this->_params;
+
+    // Some filters need special handling. Handle them here, and remove them from $params.
+    $clauses = array_merge($clauses, $this->_whereClauseSpecialParams($params));
+
     foreach ($this->_columns as $tableName => $table) {
       if (array_key_exists('filters', $table)) {
         foreach ($table['filters'] as $fieldName => $field) {
           $clause = NULL;
           if (CRM_Utils_Array::value('operatorType', $field) & CRM_Utils_Type::T_DATE) {
-            $relative = CRM_Utils_Array::value("{$fieldName}_relative", $this->_params);
-            $from     = CRM_Utils_Array::value("{$fieldName}_from", $this->_params);
-            $to       = CRM_Utils_Array::value("{$fieldName}_to", $this->_params);
+            $relative = CRM_Utils_Array::value("{$fieldName}_relative", $params);
+            $from     = CRM_Utils_Array::value("{$fieldName}_from", $params);
+            $to       = CRM_Utils_Array::value("{$fieldName}_to", $params);
 
             $clause = $this->dateClause($field['name'], $relative, $from, $to, $field['type']);
           }
           else {
-            $op = CRM_Utils_Array::value("{$fieldName}_op", $this->_params);
+            $op = CRM_Utils_Array::value("{$fieldName}_op", $params);
             if ($op) {
               $clause = $this->whereClause($field,
                 $op,
-                CRM_Utils_Array::value("{$fieldName}_value", $this->_params),
-                CRM_Utils_Array::value("{$fieldName}_min", $this->_params),
-                CRM_Utils_Array::value("{$fieldName}_max", $this->_params)
+                CRM_Utils_Array::value("{$fieldName}_value", $params),
+                CRM_Utils_Array::value("{$fieldName}_min", $params),
+                CRM_Utils_Array::value("{$fieldName}_max", $params)
               );
             }
           }
@@ -429,7 +452,6 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
   }
 
   function alterDisplay(&$rows) {
-    dsm($rows, 'rows');
     // custom code to alter rows
     $entryFound = FALSE;
     $checkList = array();
@@ -507,10 +529,9 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
   }
 
   function _set_relationship_types() {
-    // Fixme: might be worth getting these via API.
-    $this->_relationship_types = array(
-      'Household Member of' => 7,
-    );
+    foreach (CRM_Core_PseudoConstant::relationshipType() as $id => $type) {
+      $this->_relationship_types[$type['label_a_b']] = $id;
+    }
   }
 
   function _set_membership_types() {
@@ -532,5 +553,93 @@ class CRM_Jmanage_Form_Report_UpcomingYahrzeits extends CRM_Report_Form {
     require_once('utils/CustomSearchTools.php');
     $searchTools = new CustomSearchTools();
     $this->_membership_groups = $searchTools->getMembershipOrgsforSelectList();
+  }
+
+  /**
+   * Build where clause for groups.
+   * Overrides parent::whereGroupClause() because we want to join groups on the
+   * mourner, whereas parent::whereGroupClause() joins on contact (deceased).
+   *
+   * @param string $field
+   * @param mixed $value
+   * @param string $op
+   *
+   * @return string
+   */
+  public function whereGroupClause($field, $value, $op) {
+    $smartGroupQuery = "";
+
+    $group = new CRM_Contact_DAO_Group();
+    $group->is_active = 1;
+    $group->find();
+    $smartGroups = array();
+    while ($group->fetch()) {
+      if (in_array($group->id, $this->_params['gid_value']) &&
+        $group->saved_search_id
+      ) {
+        $smartGroups[] = $group->id;
+      }
+    }
+
+    CRM_Contact_BAO_GroupContactCache::check($smartGroups);
+
+    $smartGroupQuery = '';
+    if (!empty($smartGroups)) {
+      $smartGroups = implode(',', $smartGroups);
+      $smartGroupQuery = " UNION DISTINCT
+                  SELECT DISTINCT smartgroup_contact.contact_id
+                  FROM civicrm_group_contact_cache smartgroup_contact
+                  WHERE smartgroup_contact.group_id IN ({$smartGroups}) ";
+    }
+
+    $sqlOp = $this->getSQLOperator($op);
+    if (!is_array($value)) {
+      $value = array($value);
+    }
+    $clause = "{$field['dbAlias']} IN (" . implode(', ', $value) . ")";
+
+    $contactAlias = $this->_aliases['civicrm_contact_mourner'];
+    if (!empty($this->relationType) && $this->relationType == 'b_a') {
+      $contactAlias = $this->_aliases['civicrm_contact_b'];
+    }
+    return " {$contactAlias}.id {$sqlOp} (
+                          SELECT DISTINCT {$this->_aliases['civicrm_group']}.contact_id
+                          FROM civicrm_group_contact {$this->_aliases['civicrm_group']}
+                          WHERE {$clause} AND {$this->_aliases['civicrm_group']}.status = 'Added'
+                          {$smartGroupQuery} ) ";
+  }
+
+  /**
+   * Process special filters parameters which are not going to play well with
+   * $this->whereClause(); this function removes those items from $params
+   * (by reference) and handles them according to its own logic.
+   *
+   * @param <type> $params
+   * @return Array of where clauses.
+   */
+  function _whereClauseSpecialParams(&$params) {
+    $clauses = array();
+
+    $has_mourner_op = $params['has_mourner_op'];
+    $has_mourner_value = $params['has_mourner_value'];
+    unset($params['has_mourner_op']);
+    unset($params['has_mourner_value']);
+
+    $field = $this->_columns[$this->_yahrzeit_table]['filters']['has_mourner'];
+
+    $clause = NULL;
+    switch ($has_mourner_value) {
+      case 1:
+        $clause = "{$this->_aliases['civicrm_contact_mourner']}.id IS NOT NULL";
+        break;
+      case 0:
+        $clause = "{$this->_aliases['civicrm_contact_mourner']}.id IS NULL";
+        break;
+    }
+
+    if (!empty($clause)) {
+      $clauses[] = $clause;
+    }
+    return $clauses;
   }
 }
